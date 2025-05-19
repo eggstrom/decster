a051eb4f08caf157ad2180382304cc09509851f4
eggstrom <tristan65536@gmail.com>
2025-05-19 12:22:10 +0200

    Add static sources to `show` command

--- src/cli/show.rs
+++ src/cli/show.rs
@@ -2,9 +2,10 @@ use std::borrow::Cow;
 
 use anyhow::Result;
 use clap::{ArgMatches, Command, arg};
+use crossterm::style::Stylize;
 use termtree::Tree;
 
-use crate::{app::App, globs::Globs};
+use crate::{app::App, config, globs::Globs, utils::sha256::Sha256Hash};
 
 #[derive(Debug)]
 pub struct ShowCli<'a> {
@@ -34,10 +35,25 @@ impl<'a> ShowCli<'a> {
             .modules()
             .filter_map(|(name, state)| state.tree(name, &globs))
             .collect();
-        let leaves =
-            [(!modules.is_empty()).then(|| Tree::new("Modules".into()).with_leaves(modules))]
-                .into_iter()
-                .flatten();
+        let static_sources: Vec<Cow<_>> = config::static_sources()
+            .filter(|source| globs.is_match(source))
+            .map(|source| {
+                let hash = Sha256Hash::from_path(&app.env.static_source_dir().join(source));
+                let hash = match hash {
+                    Ok(hash) => hash.to_string().yellow().to_string(),
+                    Err(err) => format!("{} {err:?}", "error".red()),
+                };
+                format!("{source}: {hash}").into()
+            })
+            .collect();
+
+        let leaves = [
+            (!modules.is_empty()).then(|| Tree::new("Modules".into()).with_leaves(modules)),
+            (!static_sources.is_empty())
+                .then(|| Tree::new("Static Sources".into()).with_leaves(static_sources)),
+        ]
+        .into_iter()
+        .flatten();
         let leaves = Vec::from_iter(leaves);
 
         if !leaves.is_empty() {
